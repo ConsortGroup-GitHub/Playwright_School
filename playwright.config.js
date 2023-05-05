24089d588fe047dcc1f8564b45127af958c1eb7d
ConsortGroup-GitHub <GSD.DLG.DOSol.GitHub@consort-group.com>
2023-05-05 14:35:01 +0200

    Config Video Retain On Failure Only

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    video: 'on',
+    video: 'retain-on-failure',
     // launchOptions: {
     //   slowMo: 1000
     // },
