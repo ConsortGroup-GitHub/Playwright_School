1d39b2b398c5dedcb5c3767b7ce0dfd33dd52738
ConsortGroup-GitHub <GSD.DLG.DOSol.GitHub@consort-group.com>
2023-11-08 15:17:32 +0100

    Fixed a test and deleted another one

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -6,9 +6,6 @@ test('My first test', async ({page}) => {
 
     // await console.log('Début du traitement !')
     await page.goto('https://google.com')
-    await expect(page).toHaveTitle('Gogle')
+    await expect(page).toHaveTitle('Google')
     // await console.log('Fin du traitement !')
-    
-
-    
 })
